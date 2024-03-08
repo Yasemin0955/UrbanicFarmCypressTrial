17daa417e649694c61495b0352ff625054409ac6
yasemin <ymacit0955@gmail.com>
2024-03-08 20:57:00 +0000

    first commit

---
+++ cypress/e2e/urbanicTest/US_001.cy.js
@@ -0,0 +1,7 @@
+describe('', () => {
+    it('', () => {
+      cy.visit('https://www.google.co.uk/')  
+      cy.get('#L2AGLb > .QS5gu').click()
+      cy.get('#APjFqb').type("selam")
+    });
+});
